5c617224d0e8cfd8dd8910ec9413d91b3f330c56
王维民 <wwangaz@connect.ust.hk>
2015-08-08 17:24:12 +0800

    finish db

---
+++ app/src/main/java/model/County.java
@@ -0,0 +1,43 @@
+package model;
+
+/**
+ * Created by wangweimin on 15/8/6.
+ */
+public class County {
+    private int id;
+    private String countyCode;
+    private String countyName;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/model/Province.java
@@ -0,0 +1,34 @@
+package model;
+
+/**
+ * Created by wangweimin on 15/8/6.
+ */
+public class Province {
+    private int id;
+    private String provinceCode;
+    private String provinceName;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+}
